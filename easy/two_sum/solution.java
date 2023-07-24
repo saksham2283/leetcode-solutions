493ade940777e06020574edf0909cbaf5403b4ad
saksham2283 <saksham2233@gmail.com>
2023-07-24 18:35:14 +0000

    solve: Two Sum [Easy] in java — 2023-07-24

---
+++ easy/two_sum/solution.java
@@ -0,0 +1,50 @@
+// ============================================================
+//  Problem : Two Sum
+//  Difficulty : Easy
+//  Topics : Array, Hash Table
+//  Language : java
+//  Submitted : 2023-07-24 18:35 UTC
+//  Runtime : 48   Memory : 44152000
+//  LeetCode URL : https://leetcode.com/problems/two-sum/
+// ============================================================
+//
+// Problem Description:
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// 
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// 
+// You can return the answer in any order.
+// 
+//  
+// Example 1:
+// 
+// 
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+// 
+// 
+// Example 2:
+// 
+// 
+// Input: nums = [3,2,4], target = 6
+// Output: [1,2]
+//
+// ============================================================
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int b[]=new int[2];
+        for(int i=0;i<nums.length-1;i++){
+            for(int j=i+1;j<nums.length;j++){
+              if((nums[i]+nums[j])==target){
+                  b[0]=i;
+                  b[1]=j;
+              }
+               
+            }
+
+        }
+        return b;
+    }
+}
