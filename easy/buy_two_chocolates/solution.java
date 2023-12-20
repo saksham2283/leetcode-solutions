342ec4954218f9e32f05cdba98544887ce1bad1a
saksham2283 <saksham2233@gmail.com>
2023-12-20 15:23:00 +0000

    solve: Buy Two Chocolates [Easy] in java — 2023-12-20

---
+++ easy/buy_two_chocolates/solution.java
@@ -0,0 +1,47 @@
+// ============================================================
+//  Problem : Buy Two Chocolates
+//  Difficulty : Easy
+//  Topics : Array, Greedy, Sorting
+//  Language : java
+//  Submitted : 2023-12-20 15:23 UTC
+//  Runtime : 2   Memory : 44332000
+//  LeetCode URL : https://leetcode.com/problems/buy-two-chocolates/
+// ============================================================
+//
+// Problem Description:
+// You are given an integer array prices representing the prices of various chocolates in a store. You are also given a single integer money, which represents your initial amount of money.
+// 
+// You must buy exactly two chocolates in such a way that you still have some non-negative leftover money. You would like to minimize the sum of the prices of the two chocolates you buy.
+// 
+// Return the amount of money you will have leftover after buying the two chocolates. If there is no way for you to buy two chocolates without ending up in debt, return money. Note that the leftover must be non-negative.
+// 
+//  
+// Example 1:
+// 
+// 
+// Input: prices = [1,2,2], money = 3
+// Output: 0
+// Explanation: Purchase the chocolates priced at 1 and 2 units respectively. You will have 3 - 3 = 0 units of money afterwards. Thus, we return 0.
+// 
+// 
+// Example 2:
+// 
+// 
+// Input: prices = [3,2,3], money = 3
+// Output: 3
+//
+// ============================================================
+
+class Solution {
+    public int buyChoco(int[] prices, int money) {
+        int k=0;
+        int g=0;
+        Arrays.sort(prices);
+        k=prices[0]+prices[1];
+        if(k<=money)
+         g=money-k;
+        else 
+        g=money;
+        return g;
+    }
+}
